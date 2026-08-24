905996925bb75faaaf411cb1be913a17432f4e2a
DharsDev <dharshanpalani.2006@gmail.com>
2026-08-24 22:25:18 +0530

    revert

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
-// import { MainController } from "../controllers/main.js";
+import { MainController } from "../controllers/main.js";
 
 const mainRouter = Router();
-// const mainController = new MainController();
+const mainController = new MainController();
 
-// mainRouter.post(
-//   "/review",
-//   mainController.upload.single("pdf"),
-//   mainController.review.bind(mainController),
-// );
+mainRouter.post(
+  "/review",
+  mainController.upload.single("pdf"),
+  mainController.review.bind(mainController),
+);
 
 mainRouter.get("/hello", (_request: Request, response: Response) => {
   response.send("Yoo");
