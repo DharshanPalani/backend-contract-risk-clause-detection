deda9516773b29e05ca7c86cfc68ebc4c664b2f5
DharsDev <dharshanpalani.2006@gmail.com>
2026-08-24 22:23:21 +0530

    iii

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import dotenv from "dotenv";
 
 import type { Request, Response } from "express";
 
-// import mainRouter from "./routes/main.js";
+import mainRouter from "./routes/main.js";
 
 dotenv.config();
 
 const app = express();
 
-// app.use("/api", mainRouter);
+app.use("/api", mainRouter);
 
 app.get("/", (_request: Request, response: Response) => {
   response.send("Hello son!! 67");
