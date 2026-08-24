bb7bb5e85ef00ce5ba83d650be647d7472309f5a
DharsDev <dharshanpalani.2006@gmail.com>
2026-08-25 01:28:03 +0530

    init passport in index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,13 @@ import type { Request, Response } from "express";
 import mainRouter from "./routes/main.js";
 import authRouter from "./routes/auth.js";
 
+import passport from "passport";
+
 dotenv.config();
 
 const app = express();
 
+app.use(passport.initialize());
 app.use(
   cors({
     origin: "http://localhost:5173",
