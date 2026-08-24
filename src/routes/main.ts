3477015281d8b87230cab948b0fa9f896a5e5974
DharsDev <dharshanpalani.2006@gmail.com>
2026-08-24 22:22:40 +0530

    work

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
-import { MainController } from "../controllers/main.js";
+import type { Response } from "express";
+// import { MainController } from "../controllers/main.js";
 
 const mainRouter = Router();
-const mainController = new MainController();
+// const mainController = new MainController();
 
-mainRouter.post(
-  "/review",
-  mainController.upload.single("pdf"),
-  mainController.review.bind(mainController),
-);
+// mainRouter.post(
+//   "/review",
+//   mainController.upload.single("pdf"),
+//   mainController.review.bind(mainController),
+// );
+
+mainRouter.get("/hello", (response: Response) => {
+  response.send("Yoo");
+});
 
 export default mainRouter;
